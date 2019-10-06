81c3a1ab02bcc6e688c7628b33e2fb1c50d51b6a
Logan Hart <2loganhart@gmail.com>
2019-10-05 17:30:43 -0700

    Add utility/decorator for injecting sagas into containers

---
+++ app/src/utils/constants.js
@@ -0,0 +1,3 @@
+export const RESTART_ON_REMOUNT = "takehome/saga-injector/restart-on-remount";
+export const DAEMON = "takehome/saga-injector/daemon";
+export const ONCE_TILL_UNMOUNT = "takehome/saga-injector/once-till-unmount";
