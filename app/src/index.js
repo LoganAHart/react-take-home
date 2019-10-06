f533661f7e4bb01a4551ba163505f22ee6cdf20a
Logan Hart <2loganhart@gmail.com>
2019-10-05 17:40:13 -0700

    Hook the app up to the redux store and router in index.js

--- app/src/index.js
+++ app/src/index.js
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "containers/App";
-import * as serviceWorker from "serviceWorker";
+import { Provider } from "react-redux";
+import { ConnectedRouter } from "connected-react-router";
 
+import App from "containers/App";
+import configureStore from "./configureStore";
+import history from "utils/history";
+import * as serviceWorker from "./serviceWorker";
+
+const initialState = {};
+const store = configureStore(initialState, history);
 const MOUNT_NODE = document.getElementById("root");
-ReactDOM.render(<App />, MOUNT_NODE);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
+  MOUNT_NODE
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
